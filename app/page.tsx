c3d3e795540d0cbcdd2f452796d191d69a1a4939
Bennett Davis <bennett.davis@churchofthehighlands.com>
2025-04-02 13:48:43 -0500

    Friday

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col  row-start-2 items-center justify-center text-center">
         <p className="text-3xl">You are invited to</p>
-        <h1 className="text-8xl font-bold py-4">SHIKI FRDAY</h1>
+        <h1 className="text-8xl font-bold py-4">SHIKI FRIDAY</h1>
         <p className="text-3xl">WOOOOOOO!!!!!</p>
         <Image
           unoptimized
